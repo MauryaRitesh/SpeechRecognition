97dea6c2b31eddf07be68405f9ed60c2c29e6c97
MauryaRitesh <31176772+MauryaRitesh@users.noreply.github.com>
2017-12-07 17:44:52 +0530

    Create response.py

---
+++ response.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print ('Say Something!')
+    audio = r.listen(source)
+    print ('Done!')
+ 
+text = r.recognize_google(audio)
+
+url = "https://www.google.co.in/search?q=" + text
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text,"lxml")
+for item in soup.select(".r a"):
+    f_url = item.get('href')
+    myurl = f_url.replace(f_url[:7], '')
+    myurl = myurl.split('&')
+    myurl = myurl[0]
+    print(myurl)
+    break
+
+print ('Searching from ' + myurl)
+myurl = 'http://www.desy.de/user/projects/Physics/Relativity/SR/light_mass.html'
+f_response = requests.get(myurl)
+f_soup = BeautifulSoup(f_response.text,"lxml")
+
+print (f_soup.get_text())
