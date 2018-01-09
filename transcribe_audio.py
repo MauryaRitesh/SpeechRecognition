d18626c63364d4e515e36a1a21f3743ece1a023f
Ritesh Maurya <31176772+MauryaRitesh@users.noreply.github.com>
2018-01-09 18:21:12 +0530

    Update transcribe_audio.py

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -1,14 +1,16 @@
 import speech_recognition as sr
 
-from os import path
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "english.wav")
-
 r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
+
+audio = 'audio.wav'
+
+with sr.AudioFile(audio) as source:
     audio = r.record(source)
+    print ('Done!')
 
 try:
     text = r.recognize_google(audio)
-    print("Google Speech Recognition thinks you said " + text)
+    print (text)
+    
 except Exception as e:
     print (e)
